aef331b7679864fe5e67d2571a5c9fefee3f50d4
Jack Schuss <jackschuss@gmail.com>
2016-03-17 18:43:57 -0400

    can authorize user and return ruby has with current_user data

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,11 +1,12 @@
 require "httparty"
+require 'json'
 
 class Kele
 include HTTParty
     attr_reader :auth_token
 
   def initialize(email, password)
-    response = self.class.post(base_api_endpoint + "sessions", values: {
+    response = self.class.post(base_api_endpoint + "sessions", body: {
       email: email,
       password: password
     })
@@ -15,6 +16,12 @@ include HTTParty
 
   def base_api_endpoint
     "https://www.bloc.io/api/v1/"
+  end
+
+  def get_me
+    response = self.class.get(base_api_endpoint + "users/me", headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
+    return body
   end
 
 end
