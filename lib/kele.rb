b84167b410890eaa92078993ae14048362493fd9
Jack Schuss <jackschuss@gmail.com>
2016-03-17 21:25:30 -0400

    add get_roadmap and get_Checkpoint

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,11 @@
 require "httparty"
 require 'json'
+require 'roadmap'
 
 class Kele
 include HTTParty
+include Roadmap
+
     attr_reader :auth_token
 
   def initialize(email, password)
@@ -14,9 +17,6 @@ include HTTParty
     @auth_token = response["auth_token"]
   end
 
-  def base_api_endpoint
-    "https://www.bloc.io/api/v1/"
-  end
 
   def get_me
     response = self.class.get(base_api_endpoint + "users/me", headers: { "authorization" => @auth_token })
@@ -25,10 +25,9 @@ include HTTParty
   end
 
   def get_mentor_availability
-    student_info = self.class.get(base_api_endpoint + "users/me", headers: { "authorization" => @auth_token })
-    body = JSON.parse(student_info.body)
-    id = body["current_enrollment"]["mentor_id"]
+    id = student_info("mentor_id")
     response = self.class.get(base_api_endpoint + "mentors/#{id}/student_availability", headers: { "authorization" => @auth_token })
   end
+
 
 end
