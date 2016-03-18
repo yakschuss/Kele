ea1ecd78b6f521d4d014facd46e7bf956b1a079c
Jack Schuss <jackschuss@gmail.com>
2016-03-17 23:16:13 -0400

    retrieve all messages or individual message pages

--- lib/kele.rb
+++ lib/kele.rb
@@ -29,5 +29,19 @@ include Roadmap
     response = self.class.get(base_api_endpoint + "mentors/#{id}/student_availability", headers: { "authorization" => @auth_token })
   end
 
+  def get_messages(arg = nil)
+    if arg.nil? #if no page number specified - get total number of messages
+      response = self.class.get(base_api_endpoint + "message_threads", headers: { "authorization" => @auth_token })
+      #make as many requests as there are pages, +1 to round up
+      pages = (1..(response["count"]/10 + 1)).map do |n|
+             self.class.get(base_api_endpoint + "message_threads", body: { page: n }, headers: { "authorization" => @auth_token })
+          end
+      pages
+    else
+      response = self.class.get(base_api_endpoint + "message_threads", body: { page: arg }, headers: { "authorization" => @auth_token })
+      body = JSON.parse(response.body)
+    end
+
+  end
 
 end
